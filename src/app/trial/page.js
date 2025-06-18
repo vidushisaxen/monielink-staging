c39391503770a7c5c139363f6bac0c9a9ad65951
Vidushi Saxena <vidushisaxenabdn786@gmail.com>
2025-06-18 18:43:22 +0530

    update

--- src/app/trial/page.js
+++ src/app/trial/page.js
@@ -1,16 +1,12 @@
+import Products from '@/components/Home/Products'
 import React from 'react'
-import FAQs from '../../components/Home/FAQs'
-import Solutions2 from '@/components/Home/Solutions2'
-import Solutions3 from '@/components/Home/Solutions3'
-import Products from '@/components/Home/Products2'
 
 const page = () => {
   return (
    <>
-  {/* <Solutions2/> */}
-  <Solutions3/>
+   <Products/>
+  
 
-  {/* <Products/> */}
    </>
   )
 }
